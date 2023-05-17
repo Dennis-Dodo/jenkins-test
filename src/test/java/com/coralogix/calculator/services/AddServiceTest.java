0a606c9b2a2a7940ddd7a5efee41a326c9445c46
Dennis-Dodo <68123568+Dennis-Dodo@users.noreply.github.com>
2023-05-17 02:29:43 +0200

    Update AddServiceTest.java

--- src/test/java/com/coralogix/calculator/services/AddServiceTest.java
+++ src/test/java/com/coralogix/calculator/services/AddServiceTest.java
@@ -8,24 +8,24 @@ public class AddServiceTest {
 
     @Test
     public void addIntegers1() {
-        // given
+        
         AddService service = new AddService();
 
-        // when
+       
         Result result = service.add(2, 3);
 
-        // then
+        
         assertEquals(result.getResult(), 5);
     }
      @Test
     public void addIntegers2() {
-        // given
+        
         AddService service = new AddService();
 
-        // when
+        
         Result result = service.add(8, 12);
 
-        // then
+        
         assertEquals(result.getResult(), 20);
     }
 
